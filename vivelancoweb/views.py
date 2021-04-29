5bfd097a00efa65f3751a4f3a211e779705967da
paulpopaul <hallo.paul@icloud.com>
2021-04-29 01:44:41 -0400

    clima

--- vivelancoweb/views.py
+++ vivelancoweb/views.py
@@ -14,9 +14,20 @@ def home(request):
 
 def clima(request):
 	import requests
-	html_content = requests.get(f'https://www.google.com/search?q=clima+lanco').content
+
+	USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+	LANGUAGE = "es-CL,es;q=0.5"
+	session = requests.Session()
+	session.headers['User-Agent'] = USER_AGENT
+	session.headers['Accept-Language'] = LANGUAGE
+	session.headers['Content-Language'] = LANGUAGE
+
+	html_content = session.get(f'https://www.google.cl/search?q=clima+lanco').content
 	from bs4 import BeautifulSoup
 	soup = BeautifulSoup(html_content, 'html.parser')
+
+
+
 	clima = dict()
 	clima['region'] = soup.find("span", attrs={"class": "BNeawe tAd8D AP7Wnd"}).text
 	clima['temp_now'] = soup.find("div", attrs={"class": "BNeawe iBp4i AP7Wnd"}).text
